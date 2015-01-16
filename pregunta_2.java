88eb5724e4e2423a965d825c4d837b31ba329095
joelneitor <the_unforgiven_1993@hotmail.com>
2015-01-15 20:23:14 -0430

    Delete pregunta_2.java

--- pregunta_2.java
+++
@@ -1,5 +0,0 @@
-Utilizando el diccionario SOWPODS de Scrabble (https://scrabblehelper.googlecode.com/
-svn-history/r20/trunk/ScrabbleHelper/src/dictionaries/sowpods.txt) escriba un programa (en
-su lenguaje de preferencia) que determine cuáles letras nunca aparecen repetidas de forma
-consecutiva (AA, BB, CC, etc). Por ejemplo, la letra “D” aparece repetida pues la palabra
-“ADD” contiene la sub-secuencia “DD”.
